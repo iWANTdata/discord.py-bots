e0861e1d868857d4928f5f2dfcbd58a5d2bbaf6a
Fynnyx <82290082+Fynnyx@users.noreply.github.com>
2021-06-01 11:06:22 +0200

    started commandbot

---
+++ commandbot/commandbot.py
@@ -0,0 +1,31 @@
+'''
+# ------------------------------------------------------------------------------------------------------------------------------------
+Author: Fynn Westerath
+Last Change: 26.05.2021
+(c) Copyright. Not for commercial use. All rights reserved
+GitHub
+https://github.com/Fynnyx/discord.py-bots
+# ------------------------------------------------------------------------------------------------------------------------------------
+'''
+
+
+# Imports
+import discord
+from discord.utils import get
+
+# gets the Token from .env (more infos in README and .env.example)
+f = open(".env")
+TOKEN = f.read()
+
+# variables to change
+electionbot_prefix: str = '!commandbot'
+
+# creates the class for the Election bot
+class CommandBot(discord.Client):
+
+    async def on_ready(self):
+        print('CommandBot: logged in')
+
+
+client = CommandBot()
+client.run()
