b8923c4a8721184a654d24c8c4e5192efbf75c9c
Fynnyx <82290082+Fynnyx@users.noreply.github.com>
2021-06-07 15:43:05 +0200

    update bbot

--- bbot/bbot.py
+++ bbot/bbot.py
@@ -8,3 +8,32 @@ https://github.com/Fynnyx/discord.py-bots
 # -----------------------------------------------------------------------------------------------------------------------------------
 '''
 
+# Imports
+import discord
+from discord import embeds
+from discord.utils import get
+import json
+
+# gets the Token from .env (more infos in README and .env.example)
+f = open(".env")
+TOKEN = f.read()
+
+# variables to change
+bbot_channel: int = 841768678940213270
+bbot_prefix: str = '!bbot'
+
+class Bbot(discord.Client):
+
+    async def on_ready(self):
+        print('Bbot: logged in')
+
+    async def on_message(self, message):
+        if message.content == bbot_prefix + ' help':
+            print('help')
+
+        if message.content == bbot_prefix + ' textures':
+            print('textures')
+
+        if message.content.startswith(bbot_prefix + ' add'):
+
+            print('add')
