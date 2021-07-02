caf5ef090a259ab3067363da82d4e2a0c66f20ca
Fynnyx <westerathf@gmail.com>
2021-07-02 23:47:19 +0200

    Update alteriabot.py

--- alteriabot/alteriabot.py
+++ alteriabot/alteriabot.py
@@ -23,8 +23,6 @@ TOKEN = f.read()
 alteria_bewerber_channel = 859847836182380584
 alteria_test_channel = 860451431591968768
 alteria_prefix: str = '?'
-alteria_staff : list= []
-alteria_staff_role : str = 'Alteria Staff'
 
 class Alteriabot(discord.Client):
 
@@ -126,31 +124,31 @@ class Alteriabot(discord.Client):
                     if str(role) == 'Owner':
                         await self.send_bewerbung('opened')
 
-            # if message.content.startswith(alteria_prefix + 'bewerbung '):
-            #     bew_message = str(message.content)
-            #     bew_message_list = bew_message.split(' ', 1)
-            #
-            #     if len(bew_message_list) > 0:
-            #
-            #         bew_text = str(bew_message_list[1])
-            #         bewerber = str(message.author.name)
-            #         bewerber_profile_picture = str(message.author.avatar_url)
-            #
-            #         bewerbung_embed = discord.Embed(title='Neue Bewerbung', colour=discord.Colour(self.embed_color))
-            #         bewerbung_embed.add_field(name='Discordname', value=bewerber)
-            #         bewerbung_embed.add_field(name='Bewerbertext' ,value=bew_text)
-            #         bewerbung_embed.set_thumbnail(url=bewerber_profile_picture)
-            #
-            #         bewerbungs_channel = discord.utils.get(client.get_all_channels(), id=alteria_bewerber_channel)
-            #
-            #         await bewerbungs_channel.send(embed=bewerbung_embed)
-            #
-            #         await channel.send('✅ Deine Bewerbung ist eingetroffen wenn du nichts von uns höhrst sollte alles gut sein.\n'
-            #                             'Sonst melden wir uns bei dir und du bewirbst dich nochmal')
-            #     else:
-            #         await channel.send(
-            #             '❌ Beim versuch deine Bewerbung zu senden ist was schiefgelaufen probiere ...\n'
-            #             ' `?bewerbung <bewerbertext [Alter] [Warum möchtest du mitmachen] [Gutes Mikrfon?] [Deine Erfahrung mit solchen Projekten] und [Etwas über dich]>`')
+            if message.content.startswith(alteria_prefix + 'bewerbung '):
+                bew_message = str(message.content)
+                bew_message_list = bew_message.split(' ', 1)
+
+                if len(bew_message_list) > 0:
+
+                    bew_text = str(bew_message_list[1])
+                    bewerber = str(message.author.name)
+                    bewerber_profile_picture = str(message.author.avatar_url)
+
+                    bewerbung_embed = discord.Embed(title='Neue Bewerbung', colour=discord.Colour(self.embed_color))
+                    bewerbung_embed.add_field(name='Discordname', value=bewerber)
+                    bewerbung_embed.add_field(name='Bewerbertext' ,value=bew_text)
+                    bewerbung_embed.set_thumbnail(url=bewerber_profile_picture)
+
+                    bewerbungs_channel = discord.utils.get(client.get_all_channels(), id=alteria_bewerber_channel)
+
+                    await bewerbungs_channel.send(embed=bewerbung_embed)
+
+                    await channel.send('✅ Deine Bewerbung ist eingetroffen wenn du nichts von uns höhrst sollte alles gut sein.\n'
+                                        'Sonst melden wir uns bei dir und du bewirbst dich nochmal')
+                else:
+                    await channel.send(
+                        '❌ Beim versuch deine Bewerbung zu senden ist was schiefgelaufen probiere ...\n'
+                        ' `?bewerbung <bewerbertext [Alter] [Warum möchtest du mitmachen] [Gutes Mikrfon?] [Deine Erfahrung mit solchen Projekten] und [Etwas über dich]>`')
 
 
 
